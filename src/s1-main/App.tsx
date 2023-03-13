05c27d600b3487451f8c25a14514390b0fb03895
Alex <cool.pomolov@ya.ru>
2023-03-13 18:25:47 +0300

    feat: 1 HW

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className={s.App}>
             <HW1/>
-            awdawdawdawddawawdwadw
+
             {/*раскомментировать по ходу выполнения*/}
             {/*<HW2/>*/}
             {/*<HW3/>*/}
